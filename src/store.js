735cbf07f05ca56cd5fa95a46ca2a7a78340bf4b
ChauhanShivang <chauhanshivang2003@gmail.com>
2024-12-29 14:14:21 +0530

    Initial Setup Complete

---
+++ src/store.js
@@ -0,0 +1,10 @@
+// Copied From    https://redux-toolkit.js.org/tutorials/quick-start
+
+import { configureStore } from '@reduxjs/toolkit'
+import  pasteReducer  from './redux/pasteSlice'
+
+export const store = configureStore({
+  reducer: {
+    paste: pasteReducer,
+  },
+})
